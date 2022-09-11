008572c281ccbb5b86f3a6ef9ba0065219c2fd85
Antonio Henrique Nazar <112439695+Antonio-Henrique-Nazar@users.noreply.github.com>
2022-09-11 00:24:23 -0300

    Final da primeira aula

---
+++ Assets/Scripts/Ola.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Ola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("coé");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Bobo");
+    }
+}
