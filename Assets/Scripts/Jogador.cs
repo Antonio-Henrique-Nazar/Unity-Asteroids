9446798131f227d13145b416cd3274ca199207e0
Antonio Henrique Nazar <112439695+Antonio-Henrique-Nazar@users.noreply.github.com>
2022-09-14 22:41:44 -0300

    Até layer

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -6,8 +6,18 @@ public class Jogador : MonoBehaviour
     public float aceleracao;
     public float veolcidadeAngular = 180.0f;
     public float velocidadeMaxima = 5.0f;
+    public Rigidbody2D prefabProjetil;
+    public float velocidadeprojetil= 10.0f;
     void Start()
     {
+        
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Rigidbody2D projetil = Instantiate(prefabProjetil,RigidbodyJogador.position,Quaternion.identity);
+            projetil.velocity = transform.up*velocidadeprojetil;
+        }
         
     }
 
@@ -27,5 +37,10 @@ public class Jogador : MonoBehaviour
             RigidbodyJogador.velocity = Vector2.ClampMagnitude(RigidbodyJogador.velocity,
             velocidadeMaxima);
         }
+    }
+    void OnTriggerEnter2D(Collider2D outro){
+        
+        Destroy(gameObject);
+        Debug.Log("Fui destruido");
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,5 +17,5 @@ public class Spawner : MonoBehaviour
     {
         
     }
-    void 
+    
 }
